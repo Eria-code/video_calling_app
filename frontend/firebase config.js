cd95c53d1bbd91fbb1abdcfdf893e48cd57d71f4
Eria-code <rogerseria9@gmail.com>
2025-01-13 12:50:13 +0300

    Update frontend

---
+++ frontend/firebase config.js
@@ -0,0 +1,16 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCnLuM-TEQxVGS428v_YZvk5Fa31LkgpRY",
+  authDomain: "video-chat-app-91b76.firebaseapp.com",
+  databaseURL: "https://video-chat-app-91b76-default-rtdb.firebaseio.com",
+  projectId: "video-chat-app-91b76",
+  storageBucket: "video-chat-app-91b76.firebasestorage.app",
+  messagingSenderId: "772450783434",
+  appId: "1:772450783434:web:3b86f91c44d81804db6433",
+  measurementId: "G-EFWMVCZHZD"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
